a97eecb25867fdfdefa5f867a755158d0f7f612e
Andrew Fox <foxostro@gmail.com>
2012-03-16 12:06:10 -0700

    Initial commit.

---
+++ GutsyStorm/GutsyStormAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  GutsyStormAppDelegate.h
+//  GutsyStorm
+//
+//  Created by Andrew Fox on 3/16/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GutsyStormAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GutsyStorm/GutsyStormAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  GutsyStormAppDelegate.m
+//  GutsyStorm
+//
+//  Created by Andrew Fox on 3/16/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GutsyStormAppDelegate.h"
+
+@implementation GutsyStormAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
