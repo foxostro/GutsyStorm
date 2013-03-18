471ed5b468bad6817cc1839d00d7b095846fe3c7
foxostro <foxostro@gmail.com>
2013-03-17 22:56:51 -0700

    Disable incredibly noisy logging in DEBUG builds.

--- GutsyStorm/Voxel.h
+++ GutsyStorm/Voxel.h
@@ -10,7 +10,7 @@
 #define GutsyStorm_Voxel_h
 
 
-#ifdef DEBUG // TODO: find a better home for this macro
+#if 0 && defined(DEBUG) // TODO: find a better home for this macro
 #    define DebugLog(...) do { NSLog(__VA_ARGS__); } while(0);
 #else
 #    define DebugLog(...)
