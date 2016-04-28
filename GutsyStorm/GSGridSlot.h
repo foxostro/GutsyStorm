c73e31992f03fdd88c57ec1aaee19e8c4b2c2943
foxostro <foxostro@gmail.com>
2016-04-27 21:11:17 -0700

    Added GSGridSlot class. Not really written yet.

---
+++ GutsyStorm/GSGridSlot.h
@@ -0,0 +1,13 @@
+//
+//  GSGridSlot.h
+//  GutsyStorm
+//
+//  Created by Andrew Fox on 4/27/16.
+//  Copyright © 2016 Andrew Fox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSGridSlot : NSObject
+
+@end
---
+++ GutsyStorm/GSGridSlot.m
@@ -0,0 +1,13 @@
+//
+//  GSGridSlot.m
+//  GutsyStorm
+//
+//  Created by Andrew Fox on 4/27/16.
+//  Copyright © 2016 Andrew Fox. All rights reserved.
+//
+
+#import "GSGridSlot.h"
+
+@implementation GSGridSlot
+
+@end
