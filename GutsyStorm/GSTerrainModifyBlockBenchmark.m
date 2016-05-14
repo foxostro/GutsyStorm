d782c4a2c0e3d51c19fa15dbaa1e62170abf1b29
foxostro <foxostro@gmail.com>
2016-05-13 22:57:17 -0700

    Added a second benchmark which is a little lighter and runs faster.

--- GutsyStorm/GSTerrainModifyBlockBenchmark.m
+++ GutsyStorm/GSTerrainModifyBlockBenchmark.m
@@ -65,12 +65,20 @@ extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
     empty.type = VOXEL_TYPE_EMPTY;
     
     // Make sure chunk gets loaded before we enter start the benchmark.
-    vector_float3 p = vector_make(53.0, 54.0, 81.0);
-    for(GSVoxelNeighborIndex i = 0; i < CHUNK_NUM_NEIGHBORS; ++i)
+    vector_float3 positions[] = {
+        vector_make(53.0, 54.0, 81.0),
+        vector_make(85.0, 12.0, 137.0)
+    };
+    for(size_t i=0, n=(sizeof(positions)/sizeof(*positions)); i<n; ++i)
     {
-        vector_float3 offset = [GSNeighborhood offsetForNeighborIndex:i];
-        while(![_chunkStore nonBlockingVaoAtPoint:[GSBoxedVector boxedVectorWithVector:p+offset] createIfMissing:YES]);
-        [_chunkStore chunkVoxelsAtPoint:p];
+        vector_float3 p = positions[i];
+
+        for(GSVoxelNeighborIndex i = 0; i < CHUNK_NUM_NEIGHBORS; ++i)
+        {
+            vector_float3 offset = [GSNeighborhood offsetForNeighborIndex:i];
+            while(![_chunkStore nonBlockingVaoAtPoint:[GSBoxedVector boxedVectorWithVector:p+offset] createIfMissing:YES]);
+            [_chunkStore chunkVoxelsAtPoint:p];
+        }
     }
 }
 
@@ -81,11 +89,11 @@ extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
     _terrain = nil;
 }
 
-- (void)benchmarkPlaceAndRemove
+- (void)benchmarkDigThroughFloatingIsland
 {
     vector_float3 p = vector_make(53.0, 54.0, 81.0);
 
-    uint64_t averageTime = dispatch_benchmark(11, ^{
+    uint64_t averageTime = dispatch_benchmark(3, ^{
         
         for(p.y = 54; p.y >= 26; --p.y)
         {
@@ -102,10 +110,52 @@ extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
     NSLog(@"%s: %llu ms", __PRETTY_FUNCTION__, averageTime / NSEC_PER_MSEC);
 }
 
+- (void)benchmarkDigInTheOpen
+{
+    uint64_t averageTime = dispatch_benchmark(11, ^{
+        const size_t n = 5;
+        vector_float3 positions[n] = {
+            vector_make(85.0, 13.0, 138.0),
+            vector_make(85.0, 13.0, 137.0),
+            vector_make(85.0, 12.0, 137.0),
+            vector_make(85.0, 12.0, 136.0),
+            vector_make(85.0, 11.0, 136.0)
+        };
+
+        for(int i = 0; i < n; ++i)
+        {
+            GSTerrainModifyBlockOperation *op;
+            op = [[GSTerrainModifyBlockOperation alloc] initWithChunkStore:_chunkStore
+                                                                     block:empty
+                                                                 operation:Set
+                                                                  position:positions[i]
+                                                                   journal:nil];
+            [op main];
+        }
+
+        for(int i = n-1; i >= 0; --i)
+        {
+            GSTerrainModifyBlockOperation *op;
+            op = [[GSTerrainModifyBlockOperation alloc] initWithChunkStore:_chunkStore
+                                                                     block:cube
+                                                                 operation:Set
+                                                                  position:positions[i]
+                                                                   journal:nil];
+            [op main];
+        }
+    });
+    
+    NSLog(@"%s: %llu ms", __PRETTY_FUNCTION__, averageTime / NSEC_PER_MSEC);
+}
+
 - (void)run
 {
     [self setUp];
-    [self benchmarkPlaceAndRemove];
+    [self benchmarkDigThroughFloatingIsland];
+    [self tearDown];
+    
+    [self setUp];
+    [self benchmarkDigInTheOpen];
     [self tearDown];
 }
 
